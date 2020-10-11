6de83934d8ac48ee30c922a73e310a19740d69b5
Simon Da Vall <simon.davall@gmail.com>
2020-10-11 15:46:43 +0100

    Added unit tests from previous version

---
+++ PegasusTests/MiscTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace PegasusTests
+{
+    public class MiscTests
+    {
+        [Test]
+        public void PoorInputStringToTryParse_IntIsZero()
+        {
+            int projectId;
+            string testStr = "1,2";
+
+            int.TryParse(testStr, out projectId);
+
+            Assert.AreEqual(0, projectId);
+        }
+
+        [Test]
+        public void NullInputStringToTryParse_IntIsZero()
+        {
+            int projectId;
+            string testStr = null;
+
+            int.TryParse(testStr, out projectId);
+
+            Assert.AreEqual(0, projectId);
+        }
+    }
+}
